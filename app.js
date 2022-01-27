6087c3108e440f133aeafeb305d9d5c13d748e43
NDRBAX-Fed <andre.bacaliu@icloud.com>
2022-01-28 00:11:05 +0100

    Script updated

--- app.js
+++ app.js
@@ -3,30 +3,37 @@
 let playerName = 'John';
 let computerScore = 0;
 let playerScore = 0;
-let options = ['Rock', 'Paper', 'Scissors'];
+
+
 
 function game() {
-    let playerChoice;
-    let computerChoice;
-    this.computerSelection = function(options) {
-        computerChoice = options[Math.floor(Math.random() * options.length)];
-    };
-    this.playerSelection = function() {
-        playerChoice = prompt(`What's your choice ${playerName} ?`);
-    };
-    console.log(computerChoice);
-    console.log(playerChoice);
-}
 
-function playRound(computerSelection, playerSelection) {
-    if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
-        console.log('You loose. Rock crushes the scissors and wins.');
-    } else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
-        console.log('You loose. Paper wraps the stone and wins.');
-    } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
-        console.log('You loose. Scissors cut the paper and win.');
-    } else {
-        console.log('Tie. Try again !');
-        return game();
+    let options = ['Rock', 'Paper', 'Scissors'];
+
+    function computerSelection(options) {
+        return options[Math.floor(Math.random() * options.length)];
+    }
+    console.log(computerSelection(options));
+
+    function playerSelection() {
+        return prompt(`What's your choice ${playerName} ?`);
+    }
+    console.log(playerSelection());
+
+    function playRound(computerSelection, playerSelection) {
+        if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
+            console.log('You loose. Rock crushes the scissors and wins.');
+        } else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
+            console.log('You loose. Paper wraps the stone and wins.');
+        } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
+            console.log('You loose. Scissors cut the paper and win.');
+        } else {
+            console.log('Tie. Try again !');
+            return game();
+        }
     }
 }
+
+
+
+game();
